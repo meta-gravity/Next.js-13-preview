846111eb86d2f95904ecdb871bc572af26d96372
metagravity <metagravity0@gmail.com>
2023-09-26 16:17:25 +0100

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <main>
+            <div >
+                <h1>
+                    Papermark
+                </h1>
+                
+                <div>
+                    
+                </div>
+
+            </div>
+        </main>
+    )
+}
